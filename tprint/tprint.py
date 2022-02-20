29cc1f87ab9805651b7f986e2391c4767c2876cf
c0mpt0 <93231391+execution@users.noreply.github.com>
2022-02-20 21:00:16 +0000

    Update tprint.py

--- tprint/tprint.py
+++ tprint/tprint.py
@@ -1,1 +1,24 @@
+import sys, time, os
+try:
+    from colorama import Fore
+except ModuleNotFoundError:
+    os.system("python -m pip install colorama"))
 
+
+class tprint:
+    
+    RED = Fore.RED
+    GREEN = Fore.GREEN
+    YELLOW = Fore.YELLOW
+    CYAN = Fore.CYAN
+    BLUE = Fore.BLUE
+    WHITE = Fore.WHITE
+    MAGENTA = Fore.MAGENTA
+    BLACK = Fore.BLACK
+
+    def __init__(self, colour, text, vel):
+        for character in text + "\n":
+            sys.stdout.write(f'{colour}{character}')
+            sys.stdout.flush()
+            time.sleep(1. / vel)
+    
